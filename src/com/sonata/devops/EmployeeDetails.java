cbf66e5ef4e6b9e87bfac993f5f318d5cb9f2db5
Rammohanrmc <rammohanrao.b@sonata-software.com>
2014-12-18 19:29:33 +0530

    Update EmployeeDetails.java

--- src/com/sonata/devops/EmployeeDetails.java
+++ src/com/sonata/devops/EmployeeDetails.java
@@ -44,8 +44,8 @@ public class EmployeeDetails {
 		   public void setAge(int age) {
 		   this.age = age;
 		   }
-		  /*if(age>65){
+		  if(age >= 65){
 		   System.out.println("Not Elgible to Hike life Cycle");
-		  }*/ 
+		  }
 		   
 	}
